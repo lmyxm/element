f43a04730729c9c179996e2ec9deb38512107e59
daxiong <18381994300@qq.com>
2019-04-22 13:57:01 +0800

    添加滚动JS-vue

---
+++ vue-element/AnchorTheScroll-.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="sphere_container">
+      <div class="floor" :class="{floor_one:num==0,floor_two:num==1}"  v-for="(floorOne_num,num) in floorOne" :key="num"></div>
+
+    <div class="right_anchor">
+      <ul>
+        <li class="dot" @click="anchors(n)" v-for="(itme,n) in list" :key="n"></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      steps:0,
+      scrolltop : 0 ,//滑轮 距顶部的距离
+      list:[1,2],
+      floorOne:[1,2]
+    }
+  },
+  mounted(){
+    this.getFloorDistance();
+    this.getPulleyTopDistance();
+  },
+  methods:{
+    anchors(val){
+      for(var net = 0 ; net<= this.floorOne.length-1;net++){
+          if(val == net){
+            this.pulleyRoll(this.floorOne[net],this.scrolltop);
+          }
+      }
+    },
+    /**
+     * 滑轮 向上滚动和向下滚动的函数
+     * top是楼层距窗体顶部的距离,distance当前滚动条与窗体顶部的距离
+     * */
+    pulleyRoll(top,distance){
+      /*向下滚动*/
+      if(distance < top){
+        var small_interval = (top-distance)/50;
+        var i = 0;
+        var timer = setInterval(()=>{
+          i++;
+          // console.log(distance+= small_interval);
+          distance+= small_interval
+          document.documentElement.scrollTop = distance;
+          if(i == 50){
+            clearInterval(timer);
+          }
+        },10)
+      }
+      /*向上滚动*/
+      else if(distance > top){
+        var small_interval = (distance - top)/50;
+        var i = 0;
+        var timer = setInterval(()=>{
+          i ++;
+          // console.log(distance -= small_interval);
+          distance -= small_interval
+          document.documentElement.scrollTop = distance;
+          if(i == 50){
+            clearInterval(timer);
+          }
+        },10);
+      }
+    },
+    /**
+     * 拿到所有 楼层距窗体顶部的距离
+     * */
+    getFloorDistance(){
+      //拿到每个 楼层距窗体顶部的距离
+      for(var i=0;i<this.list.length;i++){
+        this.floorOne[i] = document.getElementsByClassName("floor")[i].offsetTop;
+        this.floorOne[i] = document.getElementsByClassName('floor')[i].offsetTop;
+      }
+    },
+    /**
+     * 滑轮滚动事件 返回滑轮距顶部的距离
+     * */
+    getPulleyTopDistance(){
+      var that = this;
+      window.onscroll = function(){
+        that.scrolltop = document.documentElement.scrollTop || document.body.scrollTop;
+      }
+    }
+  },
+  watch:{
+    //监听 滑轮滚动的值的变化 来实现自动锚点
+    scrolltop(val){
+      val += 200;
+    },
+    
+  }
+}
+</script>
+
+<style  scoped>
+
+  .floor{
+      height: 1000px;
+    }
+    .floor_one{
+      background: #ff7488;
+    }
+    .floor_two{
+      background: #b5ff8a;
+    }
+    .floor_three{
+      background: #6db9ff;
+    }
+    .floor_four{
+      background: #c277ff;
+    }
+    .floor_five{
+      background: #4139ff;
+    }
+    .no_line{
+      width: 2px;
+      height: 40px;
+      background: none;
+      text-align: center;
+      border-radius: 0;
+      margin: 0 auto;
+    }
+    .lines{
+      width: 2px;
+      height: 40px;
+      background: #fff;
+      text-align: center;
+      border-radius: 0;
+      margin: 0 auto;
+    }
+    .right_anchor{
+      position: fixed;
+      right: 30px;
+      top: 30%;
+      width: 20px;
+      height: 100px;
+      z-index: 999;
+      
+    }
+    .dot{
+          width: 15px;
+          height: 15px;
+          border-radius: 100%;
+          margin: 0 auto;
+          background: #fff;
+          margin-bottom: 50px;
+        }
+    .dot{
+          cursor: pointer;
+        }
+
+</style>
---
+++ vue-element/soct.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>选择地区滚动</div>
+</template>
+
+<script>
+	mouseupFun:function(index){
+		this.zimShow=!this.zimShow;
+		var cityChoiceBox=document.getElementById('cityChoiceBox');
+		var h3=cityChoiceBox.getElementsByTagName('h3');
+		var timer = null;	
+		function scrT(iTarget){
+			clearInterval(timer);
+			document.ontouchstart=function(){
+				clearInterval(timer);
+			}
+			timer = setInterval(function(){
+			var scrollT = document.documentElement.scrollTop || document.body.scrollTop,
+			    speed = 0;
+			speed = Math.floor((iTarget - scrollT)/5);
+			if(scrollT == iTarget){
+				clearInterval(timer);
+				}else{
+					document.documentElement.scrollTop = scrollT + speed;
+					document.body.scrollTop = scrollT + speed;
+					}
+			},30);
+		}
+		scrT(h3[index].offsetTop-50);
+	}
+</script>
+
+<style>
+</style>
